5a52c3036d106d78b7a55a8bde9f18e290d3b170
Adam <51175606+AdamLeclerc@users.noreply.github.com>
2020-11-05 15:15:23 -0800

    cleanup

--- Auto Name EC2 resources/lambda_function.py
+++ Auto Name EC2 resources/lambda_function.py
@@ -13,7 +13,7 @@ log.setLevel(logging.INFO)
 # Define boto3 connections/variables
 ec2 = boto3.resource('ec2')
 # Getting the Account ID needed to filter snapshots/AMIs
-MyAWSID = boto3.client('sts').get_caller_identity().get('Account')
+myAWSID = boto3.client('sts').get_caller_identity().get('Account')
 
 # Label applied to anything not named and un-attached
 UnattachedLabel = '- UNATTACHED - '
@@ -24,14 +24,16 @@ generic_snapshot = "Created by CreateImage"
 
 
 # Finds the AWS Tag:Name.value in a dict of tags
-def get_tag_name(all_tags):
-    if all_tags is not None:
-        for tags in all_tags:
+def get_tag_name(allTags):
+    nameTag = None
+    if allTags is not None:
+        for tags in allTags:
             if tags["Key"] == 'Name':
-                name_tag = tags["Value"]
+                nameTag = tags["Value"]
     else:
-        name_tag = no_name_label
-    return name_tag
+        nameTag = no_name_label
+        
+    return nameTag
 
 
 # get all the instances and their name tags to avoid multiple lookups
@@ -67,20 +69,20 @@ class counter:
 # AMI rename process
 def rename_amis(counter):
     log.info('[ OWNED AMI LABELING TASK STARTING ]')
-    AMIFilter = {'Name': 'owner-id', 'Values': [MyAWSID]}
-    AllAMIs = ec2.images.filter(Filters=[AMIFilter])
-    for image in AllAMIs:
+    AMIFilter = {'Name': 'owner-id', 'Values': [myAWSID]}
+    allAMIs = ec2.images.filter(Filters=[AMIFilter])
+    for image in allAMIs:
         AMIName = image.name
         dob = image.creation_date[0:10]
-        ImgName = get_tag_name(image.tags)
-        if ImgName.startswith(no_name_label) or len(ImgName) == 0:
-            AMIName += ' ' + dob
+        imgName = get_tag_name(image.tags)
+        if imgName.startswith(no_name_label) or len(imgName) == 0:
+            AMIName += f' {dob}'
             log.info(f'Labeling Image: {image.id} with {AMIName}')
-            ImgNewName = [{'Key': 'Name', 'Value': AMIName}]
-            image.create_tags(Tags=ImgNewName)
+            imgNewName = [{'Key': 'Name', 'Value': AMIName}]
+            image.create_tags(Tags=imgNewName)
             counter.add()
         else:
-            log.info(f'\t - AMI {image.id} already has a name: {ImgName}')
+            log.info(f'\t - AMI {image.id} already has a name: {imgName}')
     log.info(f'[ AMI TASK FINISHED, {counter.number} AMIS LABELED ]')
     counter.reset()
 
@@ -89,28 +91,28 @@ def rename_amis(counter):
 def rename_ebs_volumes(EC2IDs, counter):
     log.info('[ VOLUME RENAME TASK STARTING ]')
     for volume in ec2.volumes.all():
-        VolumeName = get_tag_name(volume.tags)
+        volumeName = get_tag_name(volume.tags)
         if 'in-use' in volume.state:
-            InstanceID = volume.attachments[0]['InstanceId']
-            InstMount = volume.attachments[0]['Device']
-            InstName = EC2IDs.name(InstanceID)
-            NewVolName = f'[ {InstName} ]-{InstMount}'
-            VolTagNewName = [{'Key': 'Name', 'Value': NewVolName}]
-            if VolumeName is not NewVolName:
-                volume.create_tags(Tags=VolTagNewName)
-                log.info(f'\t - EBS: {volume.id} renamed {NewVolName}')
+            instID = volume.attachments[0]['InstanceId']
+            instMount = volume.attachments[0]['Device']
+            instName = EC2IDs.name(instID)
+            newVolName = f'[ {instName} ]-{instMount}'
+            volTagNewName = [{'Key': 'Name', 'Value': newVolName}]
+            if volumeName is not newVolName:
+                volume.create_tags(Tags=volTagNewName)
+                log.info(f'\t - EBS: {volume.id} renamed {newVolName}')
                 counter.add()
             else:
-                log.info(f'\t - EBS {volume.id} named correctly: {NewVolName}')
+                log.info(f'\t - EBS {volume.id} named correctly: {newVolName}')
         if 'available' in volume.state:
-            NewVolName = UnattachedLabel + VolumeName
-            VolTagNewName = [{'Key': 'Name', 'Value': NewVolName}]
-            if not VolumeName.startswith(UnattachedLabel):
-                volume.create_tags(Tags=VolTagNewName)
-                log.info(f'\t - EBS {volume.id} renamed: {NewVolName}')
+            newVolName = UnattachedLabel + volumeName
+            volTagNewName = [{'Key': 'Name', 'Value': newVolName}]
+            if not volumeName.startswith(UnattachedLabel):
+                volume.create_tags(Tags=volTagNewName)
+                log.info(f'\t - EBS {volume.id} renamed: {newVolName}')
                 counter.add()
             else:
-                log.info(f'\t - EBS {volume.id} correctly named: {NewVolName}')
+                log.info(f'\t - EBS {volume.id} correctly named: {newVolName}')
     log.info(f'[ VOLUME TASK FINISHED, {counter.number} VOLUMES RENAMED ]')
     counter.reset()
 
@@ -129,14 +131,14 @@ def rename_interfaces(EC2IDs, counter):
                     NICNewName = 'No-Instance-ID'
             else:
                 try:
-                    NICNewName = Interface['Description']
+                    NICNewName = interface.description
                 except Exception as e:
                     NICNewName = 'non-ec2-nic'
                     log.info(f'Interface isn\'t an EC2 instance: {e}')
         if 'available' in interface.status:
             NICNewName = UnattachedLabel
-        InterfacesNewName = [{'Key': 'Name', 'Value': NICNewName}]
-        interface.create_tags(Tags=InterfacesNewName)
+        NICNewNameTag = [{'Key': 'Name', 'Value': NICNewName}]
+        interface.create_tags(Tags=NICNewNameTag)
         log.info(f'\t - Interface {interface.id} renamed {NICNewName}')
         counter.add()
     log.info(f'[ INTERFACE TASK FINISHED, {counter.number} NICS RENAMED ]')
@@ -146,37 +148,37 @@ def rename_interfaces(EC2IDs, counter):
 # Snapshot rename process
 def rename_snapshots(counter):
     log.info('[ SNAPSHOT LABELING TASK STARTING ]')
-    SnapShotFilter = {'Name': 'owner-id', 'Values': [MyAWSID]}
-    AllSnapShots = ec2.snapshots.filter(Filters=[SnapShotFilter])
-    for snapshot in AllSnapShots:
+    snapFilter = {'Name': 'owner-id', 'Values': [myAWSID]}
+    allSnapShots = ec2.snapshots.filter(Filters=[snapFilter])
+    for snapshot in allSnapShots:
         ssid = snapshot.id
         desc = snapshot.description
         dob = snapshot.start_time.strftime("%m/%d/%y")
-        SnapName = get_tag_name(snapshot.tags)
-        if SnapName.startswith(no_name_label) or len(SnapName) == 0:
-            NewSnapName = None
+        snapName = get_tag_name(snapshot.tags)
+        if snapName.startswith(no_name_label) or len(snapName) == 0:
+            newSnapName = None
             if snapshot.description.startswith(generic_snapshot):
                 if snapshot.volume_id is not None:
                     ssvid = snapshot.volume_id
                     try:
                         VolumeTags = ec2.Volume(ssvid).tags
-                        NewSnapName = get_tag_name(VolumeTags)
+                        newSnapName = get_tag_name(VolumeTags)
                     except Exception:
                         log.info(f'\t- NO CURRENT VOLUME WITH ID : {ssvid}')
-                        NewSnapName = f'Old-{ssvid}-Snapshot-{dob}'
+                        newSnapName = f'Old-{ssvid}-Snapshot-{dob}'
                 else:
-                    NewSnapName = f'CreateImage {ssvid}-Snapshot-{dob}'
+                    newSnapName = f'CreateImage {ssvid}-Snapshot-{dob}'
             else:
-                NewSnapName = desc
-            if NewSnapName:
-                log.info(f'\t- Labeling Snapashot {ssid} as {NewSnapName}')
-                SnapNewNameTag = [{'Key': 'Name', 'Value': NewSnapName}]
-                snapshot.create_tags(Tags=SnapNewNameTag)
+                newSnapName = desc
+            if newSnapName:
+                log.info(f'\t- Labeling Snapashot {ssid} as {newSnapName}')
+                snapNewNameTag = [{'Key': 'Name', 'Value': newSnapName}]
+                snapshot.create_tags(Tags=snapNewNameTag)
                 counter.add()
             else:
                 log.error(f'\t- COULD NOT DETERMINE A NAME FOR: {ssid}')
         else:
-            log.info(f'\t - Snapshot: {ssid} already tagged as {SnapName}')
+            log.info(f'\t - Snapshot: {ssid} already tagged as {snapName}')
     log.info(f'[ SNAPSHOT TASK FINISHED, {counter.number} SNAPS LABELED ]')
     counter.reset()
 
@@ -184,13 +186,13 @@ def rename_snapshots(counter):
 # Main function
 def lambda_handler(event, context):
     EC2Instances = instance_ids()
-    RenameCounter = counter()
+    renameCounter = counter()
     log.info('[ - RENAME PROGRAM STARTING - ]')
-    rename_ebs_volumes(EC2Instances, RenameCounter)
-    rename_interfaces(EC2Instances, RenameCounter)
-    rename_snapshots(RenameCounter)
-    rename_amis(RenameCounter)
-    log.info(f'[ - RENAME FINSIHED, {RenameCounter.total} OBJECTS RENAMED - ]')
+    rename_ebs_volumes(EC2Instances, renameCounter)
+    rename_interfaces(EC2Instances, renameCounter)
+    rename_snapshots(renameCounter)
+    rename_amis(renameCounter)
+    log.info(f'[ - RENAME FINSIHED, {renameCounter.total} OBJECTS RENAMED - ]')
 
 
 # Run main on load if running from the command line
