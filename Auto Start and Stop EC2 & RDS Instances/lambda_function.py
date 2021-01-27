969747bfd50711d4e4de8f4afb412940c3368885
Adam <51175606+AdamLeclerc@users.noreply.github.com>
2021-01-27 15:23:02 -0800

    Update lambda_function.py

--- Auto Start and Stop EC2 & RDS Instances/lambda_function.py
+++ Auto Start and Stop EC2 & RDS Instances/lambda_function.py
@@ -16,26 +16,29 @@ try:
     start = os.environ['START_TAG']
 except Exception:
     start = 'autoOrc-up'
+    log.info('No environment variable set for \'START_TAG\', using default')
 try:
     stop = os.environ['STOP_TAG']
 except Exception:
     stop = 'autoOrc-down'
+    log.info('No environment variable set for \'STOP_TAG\', using default')
 
-# Start instances only on weekdays? (change to False to start every day)
+# Start instances only on weekends? (set to True to start every day)
 try:
-    weekdays = os.environ['ONLY_START_WEEKDAYS']
+    weekends = os.environ['START_WEEKENDS']
 except Exception:
-    weekdays = True
+    weekends = False
+    log.info('No flag for \'START_WEEKENDS\' set')
 
 
 # Main function that lambda calls
 def lambda_handler(event, context):
-    ThisLambda = context.function_name
+    thisLambda = context.function_name
 
     # Create cloudwatch metrics for instance start/stop/failure
     def put_cloudwatch_metric(MetricName, value, process, outcome):
         cw.put_metric_data(
-            Namespace=f'{ThisLambda}-Results',
+            Namespace=f'{thisLambda}-Results',
             MetricData=[{
                 'MetricName': MetricName,
                 'Value': value,
@@ -52,9 +55,9 @@ def lambda_handler(event, context):
         session = boto3.session.Session()
         return(session.get_available_regions('ec2'))
 
-    StartTag = 'tag:' + start
-    StopTag = 'tag:' + stop
-    aws_id = boto3.client('sts').get_caller_identity().get('Account')
+    startTag = 'tag:' + start
+    stopTag = 'tag:' + stop
+    awsID = boto3.client('sts').get_caller_identity().get('Account')
 
     # Check to see if today is a weekday
     def weekday(test_date):
@@ -63,7 +66,7 @@ def lambda_handler(event, context):
         else:
             return(False)
 
-    is_weekday = weekday(datetime.datetime.now())
+    isWeekday = weekday(datetime.datetime.now())
 
     # Define a timer, used to gague shutdown time, in UTC
     timer = time.strftime("%H:%M")
@@ -71,90 +74,98 @@ def lambda_handler(event, context):
     # Set base filters for running/stopped instances, and matching orc tags
     FilterRunning = [
         {'Name': 'instance-state-name', 'Values': ['running']},
-        {'Name': StopTag, 'Values': [timer]}
-        ]
+        {'Name': stopTag, 'Values': [timer]}
+    ]
 
     FilterStopped = [
         {'Name': 'instance-state-name', 'Values': ['stopped']},
-        {'Name': StartTag, 'Values': [timer]}
-        ]
+        {'Name': startTag, 'Values': [timer]}
+    ]
 
     counter = 0
-    ErrorCounter = 0
+    errCount = 0
 
     # On initial lambda run, spawn regional AutoOrcs
     if 'REGION_NAME' not in event.keys():
-        ThisRegion = boto3.session.Session().region_name
-        log.info(f'\n[ {ThisLambda} initializing at {timer} in {ThisRegion}]')
-        for region in get_ec2_regions():
-            resource = f'arn:aws:lambda:{ThisRegion}:{aws_id}:{ThisLambda}'
-            try:
-                resp = boto3.client('lambda').invoke(
-                    FunctionName=resource,
-                    InvocationType='Event',
-                    Payload=json.dumps({'REGION_NAME': region})
-                )
-                response = True
-                log.info(f'Invoking {ThisLambda} targeting {region} region')
+        thisRegion = boto3.session.Session().region_name
+        log.info(f'\n[ {thisLambda} initializing at {timer} in {thisRegion}]')
 
-            except Exception as e:
-                log.error(f'FAILED Invoking {ThisLambda} in {region} - {e}')
+        # below regions aren't enabled, but are avilable, in US AWS by default:
+        #   Africa (Cape Town):	af-south-1
+        #   Asia Pacific (Hong Kong):	ap-east-1
+        #   Europe (Milan):	eu-south-1
+        #   Middle East (Bahrain):	me-south-1
+        notEnabled = ['me-south-1', 'ap-east-1', 'af-south-1', 'eu-south-1']
+
+        for region in get_ec2_regions():
+            if region not in notEnabled:
+                resource = f'arn:aws:lambda:{thisRegion}:{awsID}:{thisLambda}'
+                try:
+                    resp = boto3.client('lambda').invoke(
+                        FunctionName=resource,
+                        InvocationType='Event',
+                        Payload=json.dumps({'REGION_NAME': region})
+                    )
+                    response = True
+                    log.info(f'Invoked {thisLambda} targeting {region} region')
+
+                except Exception as e:
+                    log.error(f'FAILED to run {thisLambda} in {region} - {e}')
 
     # If spawned from inital Lambda run, connect to the passed REGION_NAME
     else:
-        # Define boto3 connections/variables ##################################
+        # Define boto3 connections/variables
         region = event['REGION_NAME']
-        log.info(f'\n[ {ThisLambda} start time : {timer} in {region} ]')
-        LocalSession = boto3.session.Session(region_name=region)
-        cw = LocalSession.client('cloudwatch')
-        rds = LocalSession.client('rds')
-        ec2 = LocalSession.resource('ec2')
-        # #####################################################################
+        log.info(f'\n[ {thisLambda} start time : {timer} in {region} ]')
+        localSession = boto3.session.Session(region_name=region)
+        cw = localSession.client('cloudwatch')
+        rds = localSession.client('rds')
+        ec2 = localSession.resource('ec2')
 
         # Find the name tag of an instance
         def get_ec2_instance_name(InstID):
-            InstName = None
-            unnamed_label = '(no \'name\' Tag)'
-            EC2Inst = ec2.Instance(InstID)
-            if EC2Inst.tags is not None:
-                for tags in EC2Inst.tags:
+            instName = None
+            unnamedLabel = '(no \'name\' Tag)'
+            ec2Inst = ec2.Instance(InstID)
+            if ec2Inst.tags is not None:
+                for tags in ec2Inst.tags:
                     if tags['Key'] == 'Name':
-                        InstName = tags["Value"]
-            if InstName is None or InstName == '':
-                InstName = unnamed_label
-            return(instance_name)
+                        instName = tags['Value']
+            if instName is None or instName == '':
+                instName = unnamedLabel
+            return(instName)
 
         # Get AutoOrc-down / AutoOrc-up tags on RDS instances
         def get_rds_orc_tags(ARN, phase):
-            orc_timer = ''
+            orcTimer = ''
             tags = rds.list_tags_for_resource(ResourceName=ARN)
 
             for tag in tags['TagList']:
                 if tag['Key'] == phase:
-                    orc_timer = tag['Value']
+                    orcTimer = tag['Value']
 
-            return orc_timer
+            return(orcTimer)
 
         # Find and shutdown matching EC2 instances
         try:
-            OrcInstDown = ec2.instances.filter(Filters=FilterRunning)
-            for instance in OrcInstDown:
+            orcInstDown = ec2.instances.filter(Filters=FilterRunning)
+            for instance in orcInstDown:
                 counter += 1
-                StateCode = 0
+                stateCode = 0
                 name = get_ec2_instance_name(instance.id)
                 log.info(f' - Stopping Instance-ID: {instance.id} Name : {name}')
                 resp = instance.stop()
-                StateCode = resp['StoppingInstances'][0]['CurrentState']['Code']
+                stateCode = resp['StoppingInstances'][0]['CurrentState']['Code']
 
-                if StateCode == 16:
-                    ErrorCounter += 1
-                    log.error(f'ErrorCode # {StateCode} stopping: {name}')
+                if stateCode == 16:
+                    errCount += 1
+                    log.error(f'ErrorCode # {stateCode} stopping: {name}')
 
             if (counter > 0):
-                put_cloudwatch_metric(aws_id, counter, stop, 'Success')
+                put_cloudwatch_metric(awsID, counter, stop, 'Success')
 
-            if (ErrorCounter > 0):
-                put_cloudwatch_metric(aws_id, error_counter, stop, 'Error')
+            if (errCount > 0):
+                put_cloudwatch_metric(awsID, error_counter, stop, 'Error')
                 log.error(f'x - Errors stopping {error_counter} instances')
 
             log.info(f'\t[ Stopped {counter} instances in {region} ]')
@@ -165,61 +176,65 @@ def lambda_handler(event, context):
         try:
             OrcInstUp = ec2.instances.filter(Filters=FilterStopped)
             counter = 0
-            ErrorCounter = 0
-            BadStartCodes = ['32', '48', '64', '80']
+            errCount = 0
+            badStartCodes = ['32', '48', '64', '80']
 
             # Cycle through and start tagged EC2 instances
-            if is_weekday or weekdays is False:
+            if isWeekday or weekends is True:
                 for instance in OrcInstUp:
                     counter += 1
-                    StateCode = 0
+                    stateCode = 0
                     name = get_ec2_instance_name(instance.id)
                     log.info(f'- Start Instance-ID: {instance.id}  Name: {name}')
                     resp = instance.start()
-                    StateCode = resp['StartingInstances'][0]['CurrentState']['Code']
+                    stateCode = resp['StartingInstances'][0]['CurrentState']['Code']
 
-                    if StateCode in BadStartCodes:
-                        ErrorCounter += 1
-                        log.error(f'ErrorCode # {StateCode} starting: {name}')
+                    if stateCode in badStartCodes:
+                        errCount += 1
+                        log.error(f'ErrorCode # {stateCode} starting: {name}')
 
                 if (counter > 0):
-                    put_cloudwatch_metric(aws_id, counter, start, 'Success')
+                    put_cloudwatch_metric(awsID, counter, start, 'Success')
 
-                if (ErrorCounter > 0):
-                    put_cloudwatch_metric(aws_id, error_counter, start, 'Error')
+                if (errCount > 0):
+                    put_cloudwatch_metric(awsID, error_counter, start, 'Error')
                     log.error(f'x - Errors starting {error_counter} instances')
+            else:
+                log.info('program set to not start instances on weekends!')
 
             log.info(f'\t[ Started {counter} instances in {region} ]')
         except Exception as e:
             log.error(f'Unable to start instance in {region} due to:\n{e}')
         # Cycle through all RDS instaces, starting/stopping Orc tagged ones
         try:
-            OrcRDS = rds.describe_db_instances()
+            orcRDS = rds.describe_db_instances()
             counter = 0
-            for rds_inst in OrcRDS['DBInstances']:
-                rds_name = str(rds_inst['DBInstanceIdentifier'])
-                rds_arn = str(rds_inst['DBInstanceArn'])
-                rds_status = str(rds_inst['DBInstanceStatus'])
-                rds_az_state = str(rds_inst['MultiAZ'])
+            for rdsInst in orcRDS['DBInstances']:
+                rdsName = str(rdsInst['DBInstanceIdentifier'])
+                rdsARN = str(rdsInst['DBInstanceArn'])
+                rdsStatus = str(rdsInst['DBInstanceStatus'])
+                rdsAZstate = str(rdsInst['MultiAZ'])
 
-                if is_weekday or weekdays is False:
+                if isWeekday or weekends is True:
 
-                    if rds_az_state == 'False' and rds_status == 'stopped':
-                        orc_up = get_rds_orc_tags(rds_arn, start)
+                    if rdsAZstate == 'False' and rdsStatus == 'stopped':
+                        orcUp = get_rds_orc_tags(rdsARN, start)
 
-                        if orc_up == timer:
-                            log.info(f'RDS : {rds_name} database is starting up')
-                            rds.start_db_instance(DBInstanceIdentifier=rds_name)
+                        if orcUp == timer:
+                            log.info(f'RDS : {rdsName} database is starting up')
+                            rds.start_db_instance(DBInstanceIdentifier=rdsName)
                             counter += 1
+                else:
+                    log.info('program set to not start RDS on weekends!')
 
-                if rds_az_state == 'False' and rds_status == 'available':
-                    orc_down = get_rds_orc_tags(rds_arn, stop)
+                if rdsAZstate == 'False' and rdsStatus == 'available':
+                    orcDown = get_rds_orc_tags(rdsARN, stop)
 
-                    if orc_down == timer:
-                        log.info(f'RDS: {rds_name} is shutting down now')
-                        rds.stop_db_instance(DBInstanceIdentifier=rds_name)
+                    if orcDown == timer:
+                        log.info(f'RDS: {rdsName} is shutting down now')
+                        rds.stop_db_instance(DBInstanceIdentifier=rdsName)
                         counter += 1
             log.info(f'\t[ Started & Stopped {counter} RDS DBs in {region} ]')
         except Exception as e:
             log.error(f'Unable to start/stop RDS in {region} due to:\n{e}')
-        log.info(f'[ {ThisLambda} finished in {region} ]\n')
+        log.info(f'[ {thisLambda} finished in {region} ]\n')
